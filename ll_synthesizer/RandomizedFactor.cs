51ec5e67495f9b393724f283b1ecefdf6967f1d8
tmokmss <tmokmss@c5612876-757f-4286-85ae-5229ecce2005>
2016-06-19 12:33:42 +0000

    Forgotten .cs files added
    git-svn-id: https://subversion.assembla.com/svn/mdl/ll_synthesizer/trunk@79 c5612876-757f-4286-85ae-5229ecce2005

---
+++ ll_synthesizer/RandomizedFactor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ll_synthesizer
+{
+    class RandomizedFactor
+    {
+        public int[,] Factors { get { FactorsCalced = false; return factors; } }
+        public bool FactorsCalced { private set; get; }
+
+        public static int[] MaxMin { set; get; }
+        public static double RemovalRatio { set; get; }
+        public static double AllowedError { set; get; }
+
+        Random r = new Random();
+        int[,] factors;
+        int length;
+        bool calcInProgress = false;
+
+        public RandomizedFactor(int length)
+        {
+            this.length = length;
+            FactorsCalced = false;
+        }
+
+        public async void AsyncCalcRandomizedFactor()
+        {
+            if (calcInProgress) return;
+            await Task.Run(()=>CalcRandomizedFactor());
+        }
+
+        private void CalcRandomizedFactor()
+        {
+            calcInProgress = true;
+            int num = length;
+            if (num == 1)
+                return; // there is no answer
+            FactorsCalced = false;
+            factors = new int[num, 2];
+
+            int count = 0;
+            double targetSaved = RemovalRatio;
+            int max = 50000;
+            while (!IsAllowedFactors(factors) && count < max)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    factors[i, 0] = r.Next(MaxMin[0], MaxMin[1]);   // LRFactor
+                    factors[i, 1] = r.Next(-MaxMin[3], MaxMin[3]);  // TotalFactor
+                    //factors[i, 1] = r.Next(maxmin[2], maxmin[3]);
+                }
+                count++;
+            }
+            FactorsCalced = true;
+            calcInProgress = false;
+        }
+
+        private bool IsAllowedFactors(int[,] orgfac)
+        {
+            double sumR = 0;
+            double sumL = 0;
+            double sumAbsR = 0;
+            double sumAbsL = 0;
+            for (int i = 0; i < orgfac.GetLength(0); i++)
+            {
+                int val = orgfac[i, 0];
+                double fac = (orgfac[i, 1] < 0) ? 1 : 1;
+                double facR = Math.Cos(Math.PI / 40 * val + Math.PI / 4) * orgfac[i, 1] * fac;
+                double facL = Math.Sin(Math.PI / 40 * val + Math.PI / 4) * orgfac[i, 1] * fac;
+                sumR += facR;
+                sumL += facL;
+                sumAbsR += Math.Abs(facR);
+                sumAbsL += Math.Abs(facL);
+            }
+            if (sumAbsR * sumAbsL == 0)
+                return false;
+
+            //double R = Math.Abs(sumR/sumAbsR);
+            //double L = Math.Abs(sumL/sumAbsL);
+            double R = sumR / sumAbsR;
+            double L = sumL / sumAbsL;
+
+            var target = RemovalRatio;
+            if (IsApproxTarget(R, target) && IsApproxTarget(L, target))
+                return true;
+            return false;
+        }
+
+        private bool IsApproxTarget(double val, double target)
+        {
+            if (val > target - AllowedError && val < target + AllowedError)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ ll_synthesizer/Sound/AudioReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAudio.Wave;
+
+namespace ll_synthesizer.Sound
+{
+    class AudioReader : SoundFileReader
+    {
+        private AudioFileReader afr;
+
+        public AudioReader(string path)
+        {
+            afr = new AudioFileReader(path);
+        }
+
+        public int Length
+        {
+            get { return (int)afr.Length; }
+        }
+
+        public int Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public WaveFormat WaveFormat
+        {
+             get { return afr.WaveFormat; }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read(ref byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
